72463a07e0debfa42982e8ffeadaad486b018a95
Renan Tashiro <renantashiro@hotmail.com>
2019-09-04 13:09:50 -0300

    Framework como Angular agora detecta a mudança do valor do input

--- src/content/index.js
+++ src/content/index.js
@@ -1,6 +1,8 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "input") {
-    document.querySelector(request.selector).value = request.value;
+    const input = document.querySelector(request.selector);
+    input.value = request.value;
+    input.dispatchEvent(new Event('input', { bubbles: true }));
   } else if (request.action == "click") {
     document.querySelector(request.selector).click();
   }
