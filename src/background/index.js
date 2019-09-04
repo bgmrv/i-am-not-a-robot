36c86c860fa749c08c0e23299c7062411c5e82ee
RenanTashiro <renantashiro@hotmail.com>
2019-09-03 23:11:55 -0300

    Adicionado execução de cenário de forma hardcoded

--- src/background/index.js
+++ src/background/index.js
@@ -1,15 +1,3 @@
-function executeRotine({ rotine, tab }) {
-  rotine.forEach((command) => {
-    if (command.action === 'redirect') {
-      // TODO
-    }
-  });
-}
-
-function sendMessage({ message, callback, tab }) {
-  chrome.tabs.sendMessage(tab.id, message, callback);
-}
-
 chrome.browserAction.onClicked.addListener((tab) => {
   const url = chrome.runtime.getURL("scenarios/academus/academus.json");
 
@@ -17,3 +5,34 @@ chrome.browserAction.onClicked.addListener((tab) => {
     .then((response) => response.json())
     .then((rotine) => executeRotine({ rotine, tab }));
 });
+
+function executeRotine({ rotine, tab }) {
+  console.lo
+  let resumeExecution = false;
+
+  rotine.forEach((command, index) => {
+    if (resumeExecution) {
+      return;
+    }
+
+    if (command.action === "redirect") {
+      chrome.tabs.update({ url: command.value });
+    } else if (command.action == "sleep") {
+      setTimeout(
+        () => executeRotine({ rotine: rotine.slice(index + 1), tab }), 
+        command.value * 1000
+      );
+      resumeExecution = true;
+    } else {
+      sendMessage({
+        message: { ...command },
+        callback: (response) => console.log(response),
+        tab
+      });
+    }
+  });
+}
+
+function sendMessage({ message, callback, tab }) {
+  chrome.tabs.sendMessage(tab.id, message, callback);
+}
--- src/content/index.js
+++ src/content/index.js
@@ -1,3 +1,9 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  sendResponse(document);
+  if (request.action === "input") {
+    document.querySelector(request.selector).value = request.value;
+  } else if (request.action == "click") {
+    document.querySelector(request.selector).click();
+  }
+
+  sendResponse({ status: true });
 });
